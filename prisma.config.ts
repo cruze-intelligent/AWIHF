6055ac79af3c6bc0f71fd3388087fc1fc40ce004
Mafabi Hussein <traphouse0763@gmail.com>
2026-06-24 15:08:04 +0300

    integrated infrastructure and hardened for deployment

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,11 +1,18 @@
 import { defineConfig, env } from 'prisma/config';
 
+const url = process.env.DIRECT_URL 
+  ? env('DIRECT_URL') 
+  : process.env.DATABASE_URL 
+  ? env('DATABASE_URL') 
+  : 'postgresql://postgres:postgres@localhost:5432/postgres';
+
 export default defineConfig({
   schema: 'prisma/schema.prisma',
   migrations: {
     path: 'prisma/migrations',
   },
   datasource: {
-    url: env('DIRECT_URL'),
+    url,
   },
 });
+
